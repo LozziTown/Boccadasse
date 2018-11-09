43eaa57e1255bd8a212f04ea0b8fb8fe2c158079
LozziTown <alan.zambito@gmail.com>
2018-11-10 00:58:25 +0100

    init

--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ $(document).ready(function() {
       offset: '0;' // questo offset serve a definire quanti px prima del waypoint (specificato sopra con la classe) deve azionarsi l'istruzione jquery (scritta su).
     });
     
+    $('.storia').waypoint(function(direction) {
+        if (direction == "down") {
+            $('#mobile-nav').addClass('sticky2');
+        } else {
+            $('#mobile-nav').removeClass('sticky2');
+        }
+        }, {
+      offset: '0;' // questo offset serve a definire quanti px prima del waypoint (specificato sopra con la classe) deve azionarsi l'istruzione jquery (scritta su).
+    });
 
 
      $('#div-clear').waypoint(function(direction) {
@@ -20,7 +29,7 @@ $(document).ready(function() {
                 $('#div-a-galley').removeClass('gallery');
             }
             }, {
-          offset: '450px;' // questo offset serve a definire quanti px prima del waypoint (specificato sopra con la classe) deve azionarsi l'istruzione jquery (scritta su).
+          offset: '300px;' // questo offset serve a definire quanti px prima del waypoint (specificato sopra con la classe) deve azionarsi l'istruzione jquery (scritta su).
         });
     
     $('#sec-risto').waypoint(function(direction) {
@@ -30,7 +39,7 @@ $(document).ready(function() {
                 $('#div-a-galley').removeClass('gallery');
             }
             }, {
-          offset: '300px;' // questo offset serve a definire quanti px prima del waypoint (specificato sopra con la classe) deve azionarsi l'istruzione jquery (scritta su).
+          offset: '350px;' // questo offset serve a definire quanti px prima del waypoint (specificato sopra con la classe) deve azionarsi l'istruzione jquery (scritta su).
         });
 
     
@@ -46,6 +55,32 @@ $(document).ready(function() {
                                                      relativa all'effetto che vogliamo */
     }, {
         offset:'50%'
+    });
+    
+    
+     /* MOBILE navigation */
+
+    
+     $('.a-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.a-icon i');
+        
+        nav.slideToggle(100);
+         if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+        
+    });
+    
+    $('#div-chi').waypoint(function(direction) {
+       $('#effetto-profilo').addClass('animated flipInX');  /* Si aggiunge la classe (del foglio css scaricato dal sito animate.css)
+                                                     relativa all'effetto che vogliamo */
+    }, {
+        offset:'5%'
     });
     
     
